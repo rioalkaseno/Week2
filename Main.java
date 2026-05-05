cc557f591517eb895c0756a6b8c98e2e32b93994
rioalkaseno <rioalkaseno91@gmail.com>
2026-05-05 10:32:56 +0700

    create main

--- Main.java
+++ Main.java
@@ -1,22 +1,8 @@
-//package berbeda, bukan subclass class main
-
-//import base.Produk;
-
 public class Main {
 
     public static void main(String[] args) {
-        Produk p = new Produk("Baju", 5000, 5,"Pakaian");
-        
-
-        System.out.println(p.getNama());
-
-        System.out.println(p.getHarga());
-        p.setHarga(20000);
-        System.out.println(p.getHarga());
-
-        System.out.println(p.getStok());
-        System.out.println(p.kategori);
-    
+        Eletronik mesinCuci = new Eletronik("Mesin Cuci", 2500000.0, 5, "Elektronik", 365);
+        mesinCuci.setGaransi(0);
+        mesinCuci.tampilData();
     }
-    
 }
