b874cb658bf71f4563b69febebddc2930ebce62d
rioalkaseno <rioalkaseno91@gmail.com>
2026-05-06 10:39:38 +0700

    add getting kedaluwarsa

--- Makanan.java
+++ Makanan.java
@@ -1,12 +1,16 @@
 public class Makanan extends Produk {
-    private String kadaluwarsa;
+    private String kedaluwarsa;
     private String kategori;
 
     
-    public Makanan(String nama, double harga, int stok, String kadaluwarsa, String kategori) {
+    public Makanan(String nama, double harga, int stok, String kedaluwarsa, String kategori) {
         super(nama, harga, stok);
-        this.kadaluwarsa=kadaluwarsa;
+        this.kedaluwarsa=kedaluwarsa;
         this.kategori=kategori;
+    }
+
+    public String getKedaluwarsa(){
+        return kedaluwarsa;
     }
 
 
