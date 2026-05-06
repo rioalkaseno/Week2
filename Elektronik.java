36b3774c1bddea5d03f5623ef55eafb956dbac62
rioalkaseno <rioalkaseno91@gmail.com>
2026-05-06 12:45:58 +0700

    add objek

--- Elektronik.java
+++ Elektronik.java
@@ -1,10 +1,10 @@
 
-public class Eletronik extends Produk {
+public class Elektronik extends Produk {
     private String merek;
     private int garansiTahun;
     private static final double PPN = 0.11;
 
-    public Eletronik(String nama ,String merek ,double harga, int stok, int garansiTahun) {
+    public Elektronik(String nama ,String merek ,double harga, int stok, int garansiTahun) {
         super(nama, harga, stok);
         this.merek = merek;
         this.garansiTahun = garansiTahun;
--- Main.java
+++ Main.java
@@ -3,5 +3,11 @@ public class Main {
 
     public static void main(String[] args) {
         
+        Elektronik laptop = new Elektronik("Laptop Asus","Asus",8500000,10,2);
+        Elektronik hp = new Elektronik("HP Samsung","Samsung",3200000,25,1);
+        Makanan indomie = new Makanan("Indomie Goreng", 3500, 100, "2026-12-01", "Mie Instan");
+        Makanan roti = new Makanan("Roti Tawar", 8000, 50, "2026-04-01", "Roti");
+
+
     }
 }
