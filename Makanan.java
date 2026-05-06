ab04fe27173d10501b7e93e139db99a23c4ebe85
rioalkaseno <rioalkaseno91@gmail.com>
2026-05-06 10:36:10 +0700

    add atribut class makanan dan hapus expired

--- Makanan.java
+++ Makanan.java
@@ -1,32 +1,21 @@
 public class Makanan extends Produk {
-    private int expired;
+    private String kadaluwarsa;
+    private String kategori;
 
     
     public Makanan(String nama, double harga, int stok, String kategori, int expired) {
-        super(nama, harga, stok, kategori);
-        this.expired = expired;
+        super(nama, harga, stok);
     }
 
 
     @Override
+    //abstract & override 
     public void tampilInfo() {
-        System.out.println("Makanan");
-        super.tampilInfo();
-        System.out.println("Masa expired : " + expired + " hari");
     }
 
-    public void setExpired(int expired, String type) {
+    
 
-        if (type.equals("Nasi")) {
-             if (expired < 4) {
-            System.out.println("Masa expired tidak boleh negatif");
-        } else {
-            this.expired = expired; }}
-             if (expired < 2) {
-            System.out.println("Masa expired tidak boleh negatif");
-        } else {
-            this.expired = expired;
-        }
-    }
+
+ 
 }
     
